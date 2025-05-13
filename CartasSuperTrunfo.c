6c23c9a2ccf07eca7b7cc78f0049f27144c23892
Lukkas-JiveGlitch <lukkassampaio06@gmail.com>
2025-05-13 20:15:23 +0000

    Início da Codagem do Nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,10 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Teste larissa (?)
+// Por algum motivo desde que criei o repositório têm este comentário acima, não compreendi muito bem ^
+// Criei o Código no APP Desktop do VSCode e só estou dando Update no repositório.
+
 
 int main() {
                                             //Variáveis para a Carta Número Um
